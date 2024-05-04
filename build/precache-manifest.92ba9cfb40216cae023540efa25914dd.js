f48273efabc31ede3ccdabf869bb450d683264c5
Brlr2003 <omaralsafarti03@gmail.com>
2024-05-04 17:54:01 +0200

    initial commit

---
+++ build/precache-manifest.92ba9cfb40216cae023540efa25914dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b7ef8ce7e773138fd47",
+    "url": "/demo/react/abstrak/static/css/main.3d4c6822.chunk.css"
+  },
+  {
+    "revision": "3b7ef8ce7e773138fd47",
+    "url": "/demo/react/abstrak/static/js/main.33a05a16.chunk.js"
+  },
+  {
+    "revision": "7c6b4678392a0d5c0825",
+    "url": "/demo/react/abstrak/static/js/runtime~main.b058a727.js"
+  },
+  {
+    "revision": "f754d186fc3d4e6af92a",
+    "url": "/demo/react/abstrak/static/css/2.968cb926.chunk.css"
+  },
+  {
+    "revision": "f754d186fc3d4e6af92a",
+    "url": "/demo/react/abstrak/static/js/2.1c4b7ade.chunk.js"
+  },
+  {
+    "revision": "3e1785ef074e6d621e64",
+    "url": "/demo/react/abstrak/static/js/3.09116e02.chunk.js"
+  },
+  {
+    "revision": "b9915f5824f987f5cf9be2de1bc157c1",
+    "url": "/demo/react/abstrak/index.html"
+  }
+];
